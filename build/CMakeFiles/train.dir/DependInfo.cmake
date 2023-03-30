8ae3d90deb1efbb5ebabe2a49236ff0e18fe4b59
clown911026 <5568keb67595@gmail.com>
2023-03-30 10:21:48 +0800

    change main.cpp file name to adaboost.cpp

---
+++ build/CMakeFiles/train.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/src/adaboost.cpp" "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/build/CMakeFiles/train.dir/src/adaboost.cpp.o"
+  "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/src/segment.cpp" "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/build/CMakeFiles/train.dir/src/segment.cpp.o"
+  "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/src/spot.cpp" "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/build/CMakeFiles/train.dir/src/spot.cpp.o"
+  "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/src/stump.cpp" "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/build/CMakeFiles/train.dir/src/stump.cpp.o"
+  "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/src/utility.cpp" "/home/clown911026/cs/homework/機器人專題/第二次作業/adaboost/build/CMakeFiles/train.dir/src/utility.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../ThirdParty"
+  "/usr/include/python3.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
